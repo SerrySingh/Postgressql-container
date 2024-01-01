45cb9f45313612ffad289803de9e27aba1fc01da
SERRY SINGH <123100425+SerrySingh@users.noreply.github.com>
2024-01-01 22:41:22 +0530

    Update init.sql

--- init.sql
+++ init.sql
@@ -1,4 +1,13 @@
 -- init.sql
+
+-- Create a database
 CREATE DATABASE db;
+
+-- Create a user
 CREATE USER postgres WITH PASSWORD 'postgres';
+
+-- Grant all privileges on the database to the user
 GRANT ALL PRIVILEGES ON DATABASE db TO postgres;
+
+-- Change the password for the user
+ALTER USER postgres WITH PASSWORD 'new_password';
